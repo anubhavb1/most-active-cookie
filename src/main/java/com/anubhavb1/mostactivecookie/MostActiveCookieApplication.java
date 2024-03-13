9d436239b8947a195def1d47763542e23c331e9e
anubhavb1 <anubhav.bohidar82@gmail.com>
2024-03-13 18:05:08 +0000

    Initial commit

---
+++ src/main/java/com/anubhavb1/mostactivecookie/MostActiveCookieApplication.java
@@ -0,0 +1,13 @@
+package com.anubhavb1.mostactivecookie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MostActiveCookieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MostActiveCookieApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/anubhavb1/mostactivecookie/MostActiveCookieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anubhavb1.mostactivecookie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MostActiveCookieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
